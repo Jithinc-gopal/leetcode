2e732877ee0d5234c8c5c44b6d44448c69d03ba2
Jithin <jithingopalakrishnan2004@gmail.com>
2025-10-04 10:32:24 +0530

    containduplicates

---
+++ containDuplicate.js
@@ -0,0 +1,16 @@
+var containsNearbyDuplicate = function(nums, k) {
+    for(i=0;i<nums.length;i++){
+        for(j=i+1;j<nums.length;j++){
+           if(nums[i]===nums[j]){
+             if(Math.abs(i - j) <= k){
+                return true
+             }
+
+           }
+        }
+    }
+    return false
+    
+};
+nums = [1,2,3,1,2,3], k = 2
+ console.log(containsNearbyDuplicate(nums,k))
