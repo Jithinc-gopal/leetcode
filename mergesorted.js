5d5d4ee94ec3723aad1c3a988a96bc2301a4e256
Jithin <jithingopalakrishnan2004@gmail.com>
2025-10-06 20:08:48 +0530

    cmergesorted

---
+++ mergesorted.js
@@ -0,0 +1,7 @@
+var mergeTwoLists = function(list1, list2) {
+    let b = list1.concat(list2)
+    let c = b.sort()
+    return c
+    
+}
+console.log(mergeTwoLists(list1 = [1,2,4], list2 = [1,3,4]))
