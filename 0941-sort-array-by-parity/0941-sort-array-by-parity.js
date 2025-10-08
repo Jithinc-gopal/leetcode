39863ac23177abc4cd69d7f8cc7a2765bbe60256
Jithinc-gopal <jithingopalakrishnan2004@gmail.com>
2025-10-08 15:47:20 +0530

    Time: 2 ms (67.89%), Space: 55.3 MB (99.61%) - LeetHub

---
+++ 0941-sort-array-by-parity/0941-sort-array-by-parity.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArrayByParity = function(nums) {
+let b =  nums.sort((a,b)=>{
+    if(a%2==0&&b%2!==0){
+        return -1
+    }
+     if(a%2!==0&&b%2==0){
+        return 1
+    }
+ })
+     return b
+
+    
+};
