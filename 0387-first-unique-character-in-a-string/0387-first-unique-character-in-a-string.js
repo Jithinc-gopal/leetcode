877aa2dca34dad84732f323d71d19d0150738403
Jithinc-gopal <jithingopalakrishnan2004@gmail.com>
2025-10-09 11:41:33 +0530

    Time: 45 ms (11.6%), Space: 59.1 MB (65.95%) - LeetHub

---
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function(s) {
+    for(i=0;i<s.length;i++){
+        let unique = true;
+        for(j=0;j<s.length;j++){
+            if(i!==j&&s[i]==s[j]){
+                unique = false
+                break
+            }
+        }  
+        if(unique) return i             
+    }
+     return -1
+};
