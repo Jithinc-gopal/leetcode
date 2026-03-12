779957b75a3fea49284257e9f47d3f7d6e17c19d
Jithinc-gopal <jithingopalakrishnan2004@gmail.com>
2026-03-12 15:11:03 +0530

    Time: 0 ms (100%), Space: 13.3 MB (63.93%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -0,0 +1,16 @@
+class Solution(object):
+         
+  def fizzBuzz(self,n):
+        num = []
+        
+        for i in range(1,n+1):
+            if i%3==0 and i%5 ==0:
+                num.append('FizzBuzz')
+            elif i%3==0:
+                num.append('Fizz')
+            elif i%5==0:
+                num.append('Buzz')
+            else:
+                num.append(str(i))
+                
+        return num  
