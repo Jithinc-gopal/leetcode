df2ce7194176042d974d06c1d8119e76d52f54fd
Jithinc-gopal <jithingopalakrishnan2004@gmail.com>
2026-04-01 09:41:51 +0530

    Time: 152 ms (12.12%), Space: 12.3 MB (73.74%) - LeetHub

--- 2006-count-number-of-pairs-with-absolute-difference-k/2006-count-number-of-pairs-with-absolute-difference-k.py
+++ 2006-count-number-of-pairs-with-absolute-difference-k/2006-count-number-of-pairs-with-absolute-difference-k.py
@@ -3,8 +3,8 @@ class Solution(object):
         count = 0
         for i in range(len(nums)):
             for j in range(i+1,len(nums)):
-                if abs(nums[i]-nums[j])==k:
-                    count+=1
+                if nums[i]-nums[j]==k or nums[j]-nums[i]==k:
+                      count+=1
         return count   
         
       
