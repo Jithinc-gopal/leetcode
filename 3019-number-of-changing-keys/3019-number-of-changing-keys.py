a7cc770c998435efe8e1f22fd9b3eca75cc022b2
Jithinc-gopal <jithingopalakrishnan2004@gmail.com>
2026-05-15 12:00:34 +0530

    Time: 1 ms (66.99%), Space: 12.5 MB (15.05%) - LeetHub

---
+++ 3019-number-of-changing-keys/3019-number-of-changing-keys.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def countKeyChanges(self, s):
+        count = 0
+        for i in range(len(s)-1):
+            if s[i].lower()!=s[i+1].lower():
+                count+=1
+                    
+        return count    
